f2fb10157e53d6de7241e3675b2a3aac2572ea1f
DryRun <david.renhwa.yu@gmail.com>
2016-06-02 18:43:22 +0200

    Add configuration for digis for bx 1 only

---
+++ test/crab/crab_submit_digis_isobx_cfg.py
@@ -0,0 +1,45 @@
+
+# example crab configuration file for one single run
+
+##________________________________________________________________________________||
+#Configurables
+
+#dataset = '/MinimumBias/Commissioning2016-v1/RAW'
+#dataset = '/Cosmics/Commissioning2016-v1/RAW'
+dataset = '/JetHT/Run2016B-v2/RAW'
+runs = '273150,273158,273287,273290,273291,273292,273294,273295,273296,273299'
+#run = '266150'
+#run = '268500'
+
+##________________________________________________________________________________||
+
+jobname = "HCALPFG_digis_isobx_"
+jobname += dataset[1:].replace('/','_').replace(':','_')#.replace('RAW','RAW_'+run)
+jobname += "_v1_0"
+##________________________________________________________________________________||
+
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+##________________________________________________________________________________||
+
+config.General.requestName = jobname
+config.General.workArea = 'crab_workarea'
+config.General.transferOutputs = True
+config.General.transferLogs = False
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = '/uscms/home/dryu/HCAL/CMSSW_8_0_8_patch1/src/HCALPFG/HcalTupleMaker/test/analysis_digis_isobx_cfg.py'
+config.JobType.pyCfgParams = ['outputFile=hcalTupleTree.root']
+
+config.Data.inputDataset = dataset
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'LumiBased'
+config.Data.unitsPerJob = 20
+#config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions12/8TeV/Prompt/Cert_190456-208686_8TeV_PromptReco_Collisions12_JSON.txt'
+config.Data.runRange = runs # '193093-194075'
+config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.publication = True
+config.Data.outputDatasetTag = 'HCALPFG_digis_isobx'
+
+config.Site.storageSite = "T3_US_FNALLPC"
